6e90671d63cb2f1e88384de929a4e676f40d041b
yongqingshannnn <yongqing.shann@gmail.com>
2024-06-10 21:27:42 +0800

    made changes!

--- problem_set_1.py
+++ problem_set_1.py
@@ -14,6 +14,14 @@ def bark():
   Requirements: 
     1. The proper nouns (name and breed) must be capitalized in the output, as is standard in English writing, regardless of how the user entered them.
   """
+  name = input("What is the name of your dog? ")
+  name = name.capitalize()
+  age = input("What is the age of your dog? ")
+  age = int(age)
+  breed = input("What is the breed of your dog? ")
+  breed = breed.capitalize()
+  print(f'{name}, the {age} year old {breed}, says, \"Woof!\"')
+
 
 def bark_with_validation():
   """
@@ -24,6 +32,21 @@ def bark_with_validation():
     3. If the user enters an invalid age, the string, "Age error!", must be printed and nothing else.  An invalid age is any input that is not an integer between 0 and 15, inclusive.
     4. If the user enters an invalid breed, the string, "Breed error!", must be printed and nothing else.  An invalid breed is any breed that is not in the list, ["Schnauzer", "Terrier", "Poodle", "Mastiff"]
   """
+  name = input("What is the name of your dog? ")
+  name = name.capitalize()
+  age = input("What is the age of your dog? ")
+  age = int(age)
+  breed = input("What is the breed of your dog?")
+  breed = breed.capitalize()
+  if not name.isalpha() or len(name) < 2: 
+    print ("Name error!")
+  elif age < 0 or age > 15:
+    print("Age error!")
+  elif breed not in ["Schnauzer", "Terrier", "Poodle", "Mastiff"]:
+    print("Breed error!")
+  else:
+    print(f'{name}, the {age} year old {breed}, says, \"Woof!\"')
+
 
 def respond_to_anything():
   """
@@ -35,6 +58,17 @@ def respond_to_anything():
     3. If the user enters text ending in the "!" character, print the response, "Exciting!"
     4. If the user enters text that does not include a punctuation mark at the end (punctuation marks include ".", "?", and "!"), print the response, "Please include a punctuation mark at the end of your sentence!"
   """
+  sentence = input("Enter a sentence")
+  last_character = sentence[-1]
+  if last_character == ".":
+    print("That's true.")
+  elif last_character == "?":
+    print("I'm sorry, I don't know.")
+  elif last_character == "!":
+    print("Exciting!")
+  else:
+    print("Please include a punctuation mark at the end of your sentence!")
+
 
 
 def respond_to_anything_but_nonsense():
@@ -44,3 +78,16 @@ def respond_to_anything_but_nonsense():
   Requirements:
     5. If the user includes the word, 'nonsense', anywhere in the response, regardless of capitalization, do not print any output.
   """
+  sentence = input("Enter a sentence")
+  if sentence.lower().find("nonsense") != - 1: 
+    return
+  last_character = sentence[-1]
+  if last_character == ".":
+    print("That's true.")
+  elif last_character == "?":
+    print("I'm sorry, I don't know.")
+  elif last_character == "!":
+    print("Exciting!")
+  else:
+    print("Please include a punctuation mark at the end of your sentence!")
+
--- problem_set_3.py
+++ problem_set_3.py
@@ -12,7 +12,9 @@ import random
 # This function accepts two arguments: a minimum value and a maximum value.
 # The function must return a random integer between these two values, inclusive.
 # Use the function random.randint() to generate the pseudo-random number.
-
+def get_random_int(minimum, maximum):
+    return random.randint(minimum, maximum)
+    
 
 ##--------------------- Function #2 ---------------------##
 # Define a function named 'get_guess'.
@@ -22,6 +24,20 @@ import random
 # If the user has entered an invalid response (i.e. anything that is not an integer in this range), the function returns an integer, -1.
 # If the user has guessed the random integer correctly, this function returns a boolean True.
 # If the user has guessed incorrectly, this function returns a boolean False.
+def get_guess(max_value):
+    user = input("guess a random integer between 1 and the max value.")
+    if user.isdigit():
+        user = int(user)
+    else:
+        return -1
+    computer = get_random_int(1, max_value)
+    if user < 1 or user > max_value:
+        return -1
+    elif user == computer:
+        return True 
+    else:
+        return False 
+
 
 
 ##--------------------- Function #3 ---------------------##
@@ -31,4 +47,45 @@ import random
 # Each time the user guesses, they are immediately informed whether they guessed correctly or not, with the printed output, "Correct!" or "Wrong!"
 # If at any time, the user enters an invalid response, the program immediately prints out the text, "Invalid response!" and does not print out anything further.
 # At the end, the function, assuming the user has entered all valid guesses, the program prints out the percent of guesses that user guessed correctly, following the format: "You guessed 75% of the random numbers correctly."
-
+def play_game():
+    counter = 0
+    result = get_guess(5)
+    if result == -1:
+        print("Invalid response!")
+        return
+    elif result == True:
+        print("Correct!")
+        counter = counter+1
+    else:
+        print("Wrong!")
+    result = get_guess(5)
+    if result == -1:
+        print("Invalid response!")
+        return
+    elif result == True:
+        print("Correct!")
+        counter = counter+1
+    else:
+        print("Wrong!")
+    result = get_guess(5)
+    if result == -1:
+        print("Invalid response!")
+        return
+    elif result == True:
+        print("Correct!")
+        counter = counter+1
+    else:
+        print("Wrong!")
+    result = get_guess(5)
+    if result == -1:
+        print("Invalid response!")
+        return
+    elif result == True:
+        print("Correct!")
+        counter = counter+1
+    else:
+        print("Wrong!")
+    percentage = ((counter / 4) * 100)
+    print(f"You guessed {percentage:.0f}% of the random numbers correctly.")
+    
+   
